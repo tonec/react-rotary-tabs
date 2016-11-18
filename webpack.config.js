ec99d9cd6b7618e475e44a5b1cadf3937be80657
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-18 14:34:56 +0000

    Addd webpack dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,27 @@ const APP_DIR = path.resolve(__dirname, 'src')
 
 const config = {
   entry: APP_DIR + '/index.js',
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 7700,
+    historyApiFallback: true
+  },
   output: {
     path: BUILD_DIR,
-    filename: 'react-auto-tabs.js'
+    filename: 'react-auto-tabs.js',
+    publicPath: 'http://localhost:7700/dist'
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
+        exclude: /node_modules/,
         include: APP_DIR,
         loader: 'babel'
       }
