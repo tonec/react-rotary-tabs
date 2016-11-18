6f21cf182093a7bd20706600652431db2b3aec02
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-18 15:12:41 +0000

    Added test for checking import and export of components in index.js.

---
+++ src/components/__spec__/index.spec.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import {expect} from 'chai'
+
+import { Tabs, Tab, TabsMenu, TabsMenuItem } from '../index'
+import TabsComponent from '../Tabs'
+import TabComponent from '../Tab'
+import TabsMenuComponent from '../TabsMenu'
+import TabsMenuItemComponent from '../TabsMenuItem'
+
+describe('Components index.js', () => {
+
+  it('should export all components as named exports', () => {
+    expect(Tabs).to.equal(TabsComponent)
+    expect(Tab).to.equal(TabComponent)
+    expect(TabsMenu).to.equal(TabsMenuComponent)
+    expect(TabsMenuItem).to.equal(TabsMenuItemComponent)
+  })
+
+})
